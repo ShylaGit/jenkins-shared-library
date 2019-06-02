bcdb5ee0eec79ddb994ffef9217554eec9ed606e
Shylaja Devadiga <shylaja.devadiga@whitehatsec.com>
2019-06-01 18:50:08 -0700

    add param

--- vars/validateVariable.groovy
+++ vars/validateVariable.groovy
@@ -1,5 +1,47 @@
-def call()
-{
-    echo "Hello World"
-}
+def call(String secret_name) {
+  pipeline {
+    agent {
+      any {
+        label "jenkins-slave-${UUID.randomUUID().toString()}"
+        inheritFrom 'default'
+        defaultContainer 'kapitan'
+        yaml  """
+  apiVersion: v1
+  kind: Pod
+  metadata:
+    labels:
+      kapitan-container: deploy
+  spec:
+    containers:
+    - name: kapitan
+      image: https://github.com/deepmind/kapitan.git
+      - cat
+      tty: true
+      env:
+        - name: KUBECONFIG
+          value: /etc/kubernetes/kubeconfig
+      volumeMounts:
+      - name: kubeconfig
+        mountPath: /etc/kubernetes
+    volumes:
+    - name: kubeconfig
+      secret:
+        secretName: "${secret_name}"
+  """
+      }
+    }
+    stages {
+      stage('Validate templates') {
+        steps {
+          sh '''#!/bin/bash
+          set -xe
+
+          echo "In step"
+          echo "secret_name ${secret_name}"
+          '''
+        }
+      }
+    }
+    }
+  }
 
