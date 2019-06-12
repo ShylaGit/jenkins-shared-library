93bd369c83ad8d13c7f622507d2c6ee975e83990
Shylaja Devadiga <shylaja.devadiga@whitehatsec.com>
2019-06-12 06:49:00 -0700

    test

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -3,18 +3,15 @@ def call(Map pipelineParams) {
         slackSend channel: pipelineParams.get('channel', ''),
                     color: 'good',
                     message: "${env.JOB_NAME} - #${currentBuild.number} Success after ${currentBuild.durationString.replace(' and counting', '')} (<${currentBuild.absoluteUrl}|Open>)${isPR ? "\nGitHub Pull Request Build #${env.CHANGE_ID} from ${env.CHANGE_AUTHOR}" : ''}"
-            
     }
     if ( buildResult == "FAILURE" ) {
         slackSend channel: pipelineParams.get('channel', ''),
                     color: 'danger',
                     message: "${env.JOB_NAME} - #${currentBuild.number} Success after ${currentBuild.durationString.replace(' and counting', '')} (<${currentBuild.absoluteUrl}|Open>)${isPR ? "\nGitHub Pull Request Build #${env.CHANGE_ID} from ${env.CHANGE_AUTHOR}" : ''}"
-            }
     }
     if ( buildResult == "UNSTABLE" ) {
         slackSend channel: pipelineParams.get('channel', ''),
                     color: 'warning',
                     message: "${env.JOB_NAME} - #${currentBuild.number} Success after ${currentBuild.durationString.replace(' and counting', '')} (<${currentBuild.absoluteUrl}|Open>)${isPR ? "\nGitHub Pull Request Build #${env.CHANGE_ID} from ${env.CHANGE_AUTHOR}" : ''}"
-            }
     }
 }
