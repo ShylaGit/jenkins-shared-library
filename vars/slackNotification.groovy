6147493f4542e79f39cb5b91156985722e898d11
Shylaja Devadiga <shylaja.devadiga@whitehatsec.com>
2019-06-12 06:58:23 -0700

    test

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,4 +1,4 @@
-def call(Map pipelineParams) {
+def call(Map pipelineParams, String buildResult) {
     if ( buildResult == "SUCCESS" ) {
         slackSend channel: pipelineParams.get('channel', ''),
                     color: 'good',
