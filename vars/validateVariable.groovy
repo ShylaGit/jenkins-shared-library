3422a8a9fc17d9e9212fda3cf7418af63f49ad7e
Shylaja Devadiga <shylaja.devadiga@whitehatsec.com>
2019-06-11 23:54:01 -0700

    mute

--- vars/validateVariable.groovy
+++ vars/validateVariable.groovy
@@ -44,12 +44,15 @@ def call(Map pipelineParams = [:]) {
     }
     post {
             success {
+            slackSend baseUrl: 'https://hooks.slack.com/services/', channel: 'sqa-alerts', color: 'good', message: 'Mute please', tokenCredentialId: 'slack-demo'
                 echo "Success"
             }
             failure {
+                slackSend baseUrl: 'https://hooks.slack.com/services/', channel: 'sqa-alerts', color: 'danger', message: 'Mute please', tokenCredentialId: 'slack-demo'
                 echo "Failure"
             }
             unstable {
+                slackSend baseUrl: 'https://hooks.slack.com/services/', channel: 'sqa-alerts', color: 'warning', message: 'Mute please', tokenCredentialId: 'slack-demo'
                 echo "Unstable"
             }
         }
