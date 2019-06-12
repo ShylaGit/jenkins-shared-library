bf5167a68c4f86e985bd25e3404de298869b24a0
Shylaja Devadiga <shylaja.devadiga@whitehatsec.com>
2019-06-12 00:06:12 -0700

    test

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,13 +1,13 @@
-def call(Map pipelineParams) {
-success {
+def call(String buildResult) {
+    if ( buildResult == "SUCCESS" ) {
             slackSend baseUrl: 'https://hooks.slack.com/services/', channel: 'sqa-alerts', color: 'good', message: 'Mute please', tokenCredentialId: 'slack-demo'
                 echo "Success"
             }
-            failure {
+    if ( buildResult == "FAILURE" ) {
                 slackSend baseUrl: 'https://hooks.slack.com/services/', channel: 'sqa-alerts', color: 'danger', message: 'Mute please', tokenCredentialId: 'slack-demo'
                 echo "Failure"
             }
-            unstable {
+    if ( buildResult == "UNSTABLE" ) {
                 slackSend baseUrl: 'https://hooks.slack.com/services/', channel: 'sqa-alerts', color: 'warning', message: 'Mute please', tokenCredentialId: 'slack-demo'
                 echo "Unstable"
             }
