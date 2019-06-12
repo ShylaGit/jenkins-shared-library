a74c33b4bd1df20296c48ff848c5fb567a6faafd
Shylaja Devadiga <shylaja.devadiga@whitehatsec.com>
2019-06-11 22:44:35 -0700

    testtttt

--- vars/validateVariable.groovy
+++ vars/validateVariable.groovy
@@ -47,22 +47,22 @@ def call(String environment) {
                 slackSend channel: pipelineParams.get('channel', ''),
                 baseUrl: 'https://hooks.slack.com/services/',
                 color: 'good',
+                tokenCredentialId: 'slack-demo',
                 message: "${env.JOB_NAME} - #${currentBuild.number} Success after ${currentBuild.durationString.replace(' and counting', '')} (<${currentBuild.absoluteUrl}|Open>)${isPR ? "\nGitHub Pull Request Build #${env.CHANGE_ID} from ${env.CHANGE_AUTHOR}" : ''}"
-                tokenCredentialId: 'slack-demo'
             }
             failure {
                 slackSend channel: pipelineParams.get('channel', ''),
                 baseUrl: 'https://hooks.slack.com/services/',
                 color: 'danger',
+                tokenCredentialId: 'slack-demo',
                 message: "${env.JOB_NAME} - #${currentBuild.number} Success after ${currentBuild.durationString.replace(' and counting', '')} (<${currentBuild.absoluteUrl}|Open>)${isPR ? "\nGitHub Pull Request Build #${env.CHANGE_ID} from ${env.CHANGE_AUTHOR}" : ''}"
-                tokenCredentialId: 'slack-demo'
             }
             unstable {
                 slackSend channel: pipelineParams.get('channel', ''),
                 baseUrl: 'https://hooks.slack.com/services/',
                 color: 'warning',
+                tokenCredentialId: 'slack-demo',
                 message: "${env.JOB_NAME} - #${currentBuild.number} Success after ${currentBuild.durationString.replace(' and counting', '')} (<${currentBuild.absoluteUrl}|Open>)${isPR ? "\nGitHub Pull Request Build #${env.CHANGE_ID} from ${env.CHANGE_AUTHOR}" : ''}"
-                tokenCredentialId: 'slack-demo'
             }
         }
   }
